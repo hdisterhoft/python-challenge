8ffb683bed549e27d6b5bfe1a7b8cc5525684ab2
Haley D <hdiste@outlook.com>
2018-10-13 22:43:19 -0500

    Second Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,29 @@
 import os
 import csv
 budget_csv = os.path.join("Resources","budget_data.csv")
+months=1
+profitlist=[]
+monthlist=[]
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     csvheader=next(csvfile)
-    months = len(next(zip(*csvreader)))
-    net = 0
+    firstrow=next(csvreader)
+    total=int(firstrow[1])
     for row in csvreader:
-        net += float(row[1])
+        months += 1
+        total += int(row[1])
+        profitlist.append(int(row[1]))
+        monthlist.append(str(row[0]))
+    v = [profitlist[i+1]-profitlist[i] for i in range(len(profitlist)-1)]
+    increase=max(v)
+    decrease=min(v)
     average_change = 0
-    increase = 0
-    decrease = 0
-    print(f"Financial Analysis \n Total Months: {months} \n Total: ${net} \n Average Change: {average_change} \n Greatest Increase in Profits: {increase} \n Greatest Decrease in Profits: {decrease} ")
+x=v.index(min(v))
+y=v.index(max(v))
+g=monthlist[x+1]
+h=monthlist[y+1]
+
+print(f"Financial Analysis \n Total Months: {months} \n Total: ${total} \n Average Change: {average_change} \n Greatest Increase in Profits: {g} ({increase}) \n Greatest Decrease in Profits: {h} ({decrease}) ")
 
     
         
